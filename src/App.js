e57b1e24964809302d73d1c4612b3b1568b17fc0
Atiti1 <atitidestiny17@gmail.com>
2022-01-20 18:48:53 +0100

    simple turing design with html and css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="main">
+      <div class="start">
+
+           <div class="leftside top">Turing</div>
+
+           <div class="rightside">Apply for jobs</div>
+
+      </div>
+
+      <div class="middle">
+
+        <div class="quick"> 
+       <p> Work full-time at top</p>
+       <p>U.S. companies</p>
+       </div>
+
+       <div class="go">
+         <p>Create your profile, pass Turing Tests and get job</p>
+         <p>offers as early as 2 weeks</p>
+         <p class="goo">Apply now</p>
+       </div>
+
+      </div>
+
+      <div class="end">
+         
+         <div class="list blu"> 
+
+         <div class="top serm">
+           Turing
+         </div>
+
+
+         <div class="serm">
+           <p class="foot one"> Customer </p>
+           <p class="one">Hire developers</p>
+           <p class="one"> Book a call</p>
+           <p class="one"> Hire for specific skills</p>
+           <p class="one"> Customer reviews</p>
+           <p class="one"> How to hire</p>
+           </div>
+     
+          <div class="serm">
+            <p class="foot two">Developers</p>
+            <p class="two">Apply for jobs</p>
+            <p class="two">Developer login</p>
+            <p class="two"> Remote Developer jobs</p>
+            <p class="two"> Developer community</p>
+            <p class="two"> Developer stories</p>
+          </div>
+
+          <div class="serm">
+             <p class="foot three"> Company</p>
+             <p class="three"> Blog</p>
+             <p class="three" >Press</p>
+             <p class="three">About us</p>
+             <p class="three">Non-developer jobs</p>
+             <p class="three"> Turing reviews</p>
+          </div>
+
+          <div class="serm">
+             <p class="foot four">Contact</p>
+             <p class="four">Contact us</p>
+             <p class="four">Help center</p>
+          </div>
+
+
+
+         </div>
+
+
+
+
+
+
+
+      </div>
+
+      
     </div>
   );
 }
